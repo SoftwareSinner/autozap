f15378118872100bf8df2454b6a879e12fbc06f5
J0HN Z0MB1E <johnzombie1992@gmail.com>
2018-07-24 13:04:10 -0400

    Added Zap Auth to login page

--- autozap.py
+++ autozap.py
@@ -5,22 +5,20 @@ import time
 from pprint import pprint
 from zapv2 import ZAPv2
 
-target = 'http://127.0.0.1' # Replace this with your target URL or IP
+target = '' # Replace this with your target URL or IP
 # Change to match the API key set in ZAP under tools menu, options, API tab. or use None if the API key is disabled.
-apikey = 'Place your API KEY HERE'
+apiKey = '' # Place your OWASP ZAP API key in the quotes 
 
 
 class OwaspZap(object):
 
-	def Api_Connect(self):
-		zap = ZAPv2(apikey=apikey)
+	def spider_login(self):
+		#spider crawling the main login page
+		zap = ZAPv2(apikey=apiKey)
 		print('[+]Accessing target {}'.format(target))
 		zap.urlopen(target)
 		time.sleep(2)
-
-	def Spider_Crawl(self):
-		zap = ZAPv2(apikey=apikey)
-		print('Spidering target {}'.format(target))
+		print('Now Spidering target {}'.format(target))
 		scanid = zap.spider.scan(target)
 		# Give the Spider a chance to start
 		time.sleep(2)
@@ -28,27 +26,68 @@ class OwaspZap(object):
 			#This will loop until the spider has finished
 			print('Spider progress %: {}'.format(zap.spider.status(scanid)))
 			time.sleep(2)
-		print('Spider crawl completed')
+		print('Spider crawl on login page completed')
 
-	def Passive_Scan(self):
-		zap = ZAPv2(apikey=apikey)
-		while(int(zap.pscan.records_to_scan) > 0):
-			print('Records to passive scan : {}'.format(zap.pscan.records_to_scan))
-			time.sleep(2)
-		print('Passive Scan completed')
-
-	def Active_Scan(self):
-		zap = ZAPv2(apikey=apikey)
-		print('Active Scanning target {}'.format(target))
+	def active_login(self):
+    	#Active scanner on login page
+		zap = ZAPv2(apikey=apiKey)
+		print('Running Active Scan {}'.format(target))
 		scanid = zap.ascan.scan(target)
 		while(int(zap.ascan.status(scanid)) < 100):
 	    		# Loop until the scanner has finished
 			print('Scan progress %: {}'.format(zap.ascan.status(scanid)))
 			time.sleep(5)
 		print('Active Scan completed')
+		zap = ZAPv2(apikey=apiKey)
+		# Reports the results
+		print('Hosts: {}'.format(', '.join(zap.core.hosts)))
+		print('Alerts: ')
+		pprint(zap.core.alerts())	
+	
+	def url_auth(self):
+		zap = ZAPv2(apikey=apiKey)
+		ctx_name = " " Place a context for the url here
+		ctx_id = zap.context.new_context(ctx_name)
+		print('zap initizalized')
+		login_url = " " # Place URL path you would like to scan here after authenticating with ZAP
+		print("Now Authenticating..")
+		zap.authentication.set_authentication_method(ctx_id, "formBasedAuthentication", "loginUrl=" + login_url)
+	
+	def spider_dashboard(self):
+    		#spider crawling the dashboard after authenticating
+		dash = " " # place dashboard page url here after authenticating into web app
+		zap = ZAPv2(apikey=apiKey)
+		ctx_name = " " # Place context here
+		ctx_id = zap.context.new_context(ctx_name)
+		print('[+]Accessing  Dashboard {}'.format(dash))
+		zap.urlopen(dash)
+		time.sleep(2)
+		print('Now Spidering target {}'.format(dash))
+		scanid = zap.spider.scan(dash,ctx_id)
+		# Give the Spider a chance to start
+		time.sleep(2)
+		while(int(zap.spider.status(scanid)) < 100):
+			#This will loop until the spider has finished
+			print('Spider progress %: {}'.format(zap.spider.status(scanid)))
+			time.sleep(2)
+		print('Spider crawl on Dashboard page completed')
 
-	def Show_Results(self):
-		zap = ZAPv2(apikey=apikey)
+
+	def active_dash(self):
+    	#Active scanner on login page
+		zap = ZAPv2(apikey=apiKey)
+		dash = " " # Place url path here
+		print('Running Active Scan {}'.format(dash))
+		scanid = zap.ascan.scan(dash)
+		while(int(zap.ascan.status(scanid)) < 100):
+	    		# Loop until the scanner has finished
+			print('Scan progress %: {}'.format(zap.ascan.status(scanid)))
+			time.sleep(5)
+		print('Active Scan on Dashboard.action completed')
+	
+
+	def show_results(self):
+		zap = ZAPv2(apikey=apiKey)
 		# Reports the results
 		print('Hosts: {}'.format(', '.join(zap.core.hosts)))
 		print('Alerts: ')
@@ -56,8 +95,9 @@ class OwaspZap(object):
 
 
 zapp = OwaspZap()
-zapp.Api_Connect()
-zapp.Spider_Crawl()
-zapp.Passive_Scan()
-zapp.Active_Scan()
-zapp.Show_Results()
+zapp.spider_login()
+zapp.active_login()
+zapp.url_auth()
+zapp.spider_dashboard()
+zapp.active_dash()
+zapp.show_results()
